f5f1b6aa99acfab9d147c6a88076339714f98820
Masa Shio <mash180sx@gmail.com>
2023-08-09 06:40:02 +0900

    add larastan

---
+++ app/Console/Commands/LarastanCommand.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Contracts\Console\Isolatable;
+use RuntimeException;
+use Symfony\Component\Process\Exception\ProcessSignaledException;
+use Symfony\Component\Process\Process;
+
+class LarastanCommand extends Command implements Isolatable
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'larastan
+        {--without-tty : Disable output to TTY}
+    ';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Run the appliation static analyze by Larastan';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->ignoreValidationErrors();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $command = ['vendor/bin/phpstan', 'analyze', '--', './'];
+
+        $process = new Process($command);
+        try {
+            $process->setTty(!$this->option('without-tty'));
+        } catch (RuntimeException $e) {
+            $this->output->writeln('Warning: ' .  $e->getMessage());
+        }
+
+        $exitCode = Command::FAILURE;
+        try {
+            $exitCode = $process->run(fn ($type, $line) => $this->output->write($line));
+        } catch (ProcessSignaledException $e) {
+            if (extension_loaded('pcntl') && $e->getSignal() !== SIGINT) {
+                throw $e;
+            }
+        }
+        $this->newLine();
+
+        return $exitCode;
+    }
+}
