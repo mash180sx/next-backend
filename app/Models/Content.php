f623ea30bac83f56ed805f98a8cfdb87870f77c2
Masa Shio <mash180sx@gmail.com>
2023-08-26 11:15:28 +0900

    modify for softdelete

--- app/Models/Content.php
+++ app/Models/Content.php
@@ -7,10 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Content extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     public function from(): BelongsTo
     {
